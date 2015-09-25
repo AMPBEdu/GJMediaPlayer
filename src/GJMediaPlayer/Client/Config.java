fd38e07278cd206fada6668cc8a6798177487f5c
Andrew Bueide <andrew.bueide2018@geringschools.net>
2015-09-24 21:11:23 -0600

    progress

---
+++ src/GJMediaPlayer/Client/Config.java
@@ -0,0 +1,32 @@
+package GJMediaPlayer.Client;
+
+import java.util.prefs.*;
+
+public class Config {
+	
+	private static Preferences config;
+	
+	private static double volume = 100.0;
+
+	public Config(){
+		config = Preferences.userRoot();
+		
+		if(config.getDouble("VOLUME", -1) != -1){
+			volume = config.getDouble("VOLUME", -1);
+		}
+	}
+	
+	public Preferences getConfig() {
+		return config;
+	}
+
+	public static double getVolume() {
+		return volume;
+	}
+
+	public void setVolume(double volume) {
+		Config.volume = volume;
+		config.putDouble("VOLUME", Config.volume);
+	}
+	
+}
--- src/GJMediaPlayer/Client/Main.java
+++ src/GJMediaPlayer/Client/Main.java
@@ -13,16 +13,16 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("FXMLCSS/Main.fxml"));
 			Scene scene = new Scene(root,600,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			scene.getStylesheets().add(getClass().getResource("FXMLCSS/application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.setResizable(false);
 			primaryStage.setTitle("GJMediaPlayer Client Alpha");
 			primaryStage.show();
 			
 		} catch(Exception e) {
-			e.printStackTrace();
+			e.printStackTrace(); 
 		}
 	}
 	
